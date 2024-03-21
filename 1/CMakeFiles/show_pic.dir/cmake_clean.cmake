741a361c14524ccc612d222a63764d9b58d6ed13
石若川 <18222478607@163.com>
2024-03-21 19:40:53 +0800

    描述描述描述描述

---
+++ 1/CMakeFiles/show_pic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/show_pic.dir/show_pic.cpp.o"
+  "show_pic.pdb"
+  "show_pic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/show_pic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
